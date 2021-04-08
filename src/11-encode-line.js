ee71599afd2724c5e8cdca20ac4a62c42548a9a5
misxka <mishutkavlesu@gmail.com>
2021-04-08 22:48:41 +0300

    feat: add solution to task-11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,31 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  const result = [];
+  result.push(arr[0]);
+  let comparing = arr[0];
+  let sum = 0;
+  for (let i = 0; i <= arr.length; i++) {
+    if (i === arr.length || arr[i] !== comparing) {
+      result.push(result[result.length - 1]);
+      if (i === arr.length) {
+        if (sum > 1) result[result.length - 2] = sum;
+        else result.pop();
+        break;
+      } else {
+        if (sum > 1) result[result.length - 2] = sum;
+        else result.pop();
+        result.push(arr[i]);
+        comparing = arr[i];
+      }
+      sum = 1;
+    } else {
+      sum++;
+    }
+  }
+  return result.join('');
 }
 
 module.exports = encodeLine;

3f462b25b088bf80b2ddc517e0a4b737c84e05ee
misxka <mishutkavlesu@gmail.com>
2021-04-08 20:00:45 +0300

    feat: add solution to task-09

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -10,8 +10,16 @@
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(n) {
+  let localN = n;
+  const digits = [];
+  while (localN > 0) {
+    digits.push(localN % 10);
+    localN = Math.trunc(localN / 10);
+  }
+  let sum = digits.reduce((acc, cur) => acc + cur);
+  if (sum > 9) sum = getSumOfDigits(sum);
+  return sum;
 }
 
 module.exports = getSumOfDigits;
